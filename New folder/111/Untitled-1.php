8d4b48b6a4a8d2b80ad8040ab81be341d9a31063
mr-aiza <aliabdoli161@gmail.com>
2021-08-28 23:03:19 +0430

    Create Untitled-1.php

---
+++ New folder/111/Untitled-1.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+</head>
+
+<body>
+  <img src="back.jpg"style="width:940px;height:190px;"/>
+	<table border="1" width="371";>
+	<caption>یازده شبکه</caption>
+		<tr>
+		<th width="73"><strong><a href="../باشگاه/استاژ شهرستان استان البرز 1.pdf">نام</a></strong></th>
+			<th width="128"></th>
+		</tr>
+	<tr>
+	<th>alireza</th>
+		<th> kian</th>
+		<th width="62">aydin</th>
+		<th width="80">morteza</th>
+	</tr>
+		
+	<tr>
+		
+	<th>abdoli</th>
+		<th>hadavand khani</th>
+		<th>azizi</th>
+		<th>salbokhi</th>
+	</tr>
+	</table>
+</body>
+</html>
